7c2afe18af7fc5a01b553a309d3b0ee5e07c039a
Nikita Lazarenko <nikitalazarenko10@gmail.com>
2023-01-19 17:53:01 +0600

    Initial commit

---
+++ src/test/java/tests/MainMenuTests.java
@@ -0,0 +1,63 @@
+package tests;
+
+
+import io.qameta.allure.Description;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import pages.Adm;
+import pages.MainPage;
+
+public class MainMenuTests extends BaseTest {
+
+    private static MainPage mainPage;
+
+    @BeforeAll
+    public static void init() {
+        mainPage = new MainPage();
+    }
+
+//    @Description("Категории на главном экране имеют текст")
+//    @Test
+//    public void testCategories10items(){
+//        List<String> categoriesNameExpected = Arrays.asList(
+//                "Каталог", "Электроника", "Одежда и обувь",
+//                "Доставка от 30 минут", "Автомобили", "Рассрочка от Ozon",
+//                "Акции", "Отели и билеты", "Красота и здоровье", "Premium");
+//        mainPage.closePopUp()
+//                .checkCategoriesHasTexts(categoriesNameExpected);
+//    }
+//
+//    @Description("Переход в другое меню делает кнопку не активной")
+//    @Test
+//    public void testGoToAnotherMenuIconNotSelected(){
+//        mainPage.closePopUp();
+//        mainPage.checkMainMenuButtonIsSelected();
+//        mainPage.openCatalog().isMainMenuIconSelected();
+//        mainPage.checkMainMenuButtonIsNotSelected();
+//    }
+//
+//    @Test
+//    @Description("Добавляем товар в корзину")
+//    public void testAddItemToCart(){
+//        String searchItem = "projector";
+//        SearchPage searchPage = mainPage.closePopUp().search(searchItem);
+//        String fullName = searchPage.getFirstFoundedFullItemName();
+//
+//        searchPage.openFirstFoundItemByTextContains()
+//                .addToCart()
+//                .openCart()
+//                .closePop()
+//                .assertItemsCount(1)
+//                .assertItemNameInCart(fullName);
+//    }
+
+    @Test
+    @Description("Авторизация")
+    public void AuthorizationMP(){
+        Adm adm = new Adm();
+        adm.authorizationMP().CheckBody();
+        adm.authorizationMP().AuthItself();
+        adm.authorizationMP().PinSetting();
+    }
+
+}
